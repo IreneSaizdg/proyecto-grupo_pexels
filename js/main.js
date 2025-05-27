ca3fd06ab5713e40ca708b09ced1287cb29307bd
mikelMondragon <mikelmondragondev@gmail.com>
2025-05-27 14:05:29 +0200

    api call completed

---
+++ js/main.js
@@ -0,0 +1,94 @@
+// VARIABLES (elementos del DOM, objetos, arrays...) -> EVENTOS -> FUNCIONES -> INVOCACIONES(a funciones)
+
+
+/*PSEUDOCODE:
+    
+*/
+
+// LLAMADAS A API -> Filtros en el fetch: Categorias, Palabras, orientación, Cantidad de imagenes, 
+//2EndPoints: (búsqueda) https://api.pexels.com/v1/search    (imagen por id para favoritos) https://api.pexels.com/v1/photos/:id
+
+
+
+
+//VARIABLES ------------------------------------------------------------------------>
+
+//Elementos del DOM
+const cardContainer = document.querySelector("#cardsContainer");
+const fragment = document.createDocumentFragment();
+
+
+
+
+//EVENTOS ------------------------------------------------------------------------->
+
+
+//FUNCIONES ----------------------------------------------------------------------->
+/**
+ * Obtener datos con el uso del endpoint Search.
+ * @param {string} query 
+ * @param {string} orientation 
+ * @param {Array} words 
+ * @returns 
+ */
+const getDataFromSearch = async (query, orientation, words) => {
+    const myString = `https://api.pexels.com/v1/search?query=${query}&orientation=${orientation}`;
+    return await obtainDataFromAPI(myString); //Cuando invocamos esta función invoca también obtainDataFromAPI con nuestra nueva URL. 
+}
+
+/**
+ * Realiza un fetch a la API de pexels con una url.
+ * @param {string} url a utilizar en la llamada a la api.
+ * @returns {Object} información devuelta por la llamada a la api.
+ */
+const obtainDataFromAPI = async (url) => {//Esta función se repite y queda automatizada.
+    try {
+        const dataAPI = await fetch(url, {//Fetch necesita ("URL", {objeto con los ajustes de petición})
+            headers: {
+                Authorization: "yyWlCwJBhQa7uDLshqAo4lPIdhSd00VEz6p5vuix6srVMfJTnXEdiEYv" //Authorization es un objeto
+            }
+        })
+        if (dataAPI.ok) {
+            const json = await dataAPI.json(); // El método .json() devuelve una promesa, por eso hay que poner el await.
+            return json;
+        } else {
+            throw "No se consiguieron las imágenes solicitadas" //Error (mandar a catch)
+        }
+
+    } catch (error) {
+        console.log(error); //Coje la info del throw
+    }
+
+}
+
+
+/**
+ * Pinta las cards en la galeria.
+ * @param {Object} -> Array con todos los datos de los objetos photos
+ */
+const fillGallery = ({ photos }) => { // Desestructurado de (json.photos)
+    cardContainer.innerHTML = ""; //Vacía el contenedor previamente
+    photos.forEach(element => {
+        //TODO: meter contenido de card.
+        const photo = document.createElement("IMG")
+        photo.src = element.src.tiny;
+        fragment.append(photo);
+    });
+    cardContainer.append(fragment)
+
+}
+
+
+//INVOCACIONES -------------------------------------------------------------------->
+
+/**
+ * Inicializa la galeria obteniendo los datos desde la API con getDataFromSearch.
+ */
+//PROVISIONAL
+const init = async () => { //init -> Inicializa
+    const dataAPI = await getDataFromSearch("ocean", "landscape", null); //Llama a la API pasándo por parámetro el query, la orientación y las keywords.
+    fillGallery(dataAPI)//Llena la galería
+}
+
+init()
+
